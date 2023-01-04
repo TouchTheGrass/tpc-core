75dadec6ba370d914fb716853440f5e4d8161e3c
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-05 01:55:14 +0300

    style_change
    соблюден единый стиль в именовании функций, формат документирования функций (docstrings), убран закоменченный и неиспользуемый код

---
+++ app/services/chess_classes/Direction.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+"""
+Перечисление прямолинейных направлений
+используется в качестве основы для всех ходов.
+"""
+
+class Direction(Enum):
+    FORWARD="FORWARD"
+    BACKWARD="BACKWARD"
+    LEFT="LEFT"
+    RIGHT="RIGHT"
