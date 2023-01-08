c96a4f1bf788f8dc957b189477cf69c2680f0c82
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-09 00:07:44 +0300

    adding_get_game_session_status
    Добавлена реализация функции get_game_session_status, исправлены ошибки в get_pieces и в редактировании записей в базе данных

--- app/services/engine.py
+++ app/services/engine.py
@@ -9,20 +9,22 @@ from app.models.piece import GameSessionModel
 from app.models.player_game_session import PlayerGameSessionModel
 from app.models.enumerations.player_status import PlayerStatus
 from app.models.enumerations.piece_type import PieceType
-
+from app.models.enumerations.piece_color import PieceColor
+from app.models.player import PlayerModel
 
 class EngineService:
 
-    def create_game_session(self) -> GameSessionEntity:
-        # TODO
-        raise NotImplementedError
 
-    def get_game_session_status(self) -> GameSessionStatus:
-        # TODO
-        raise NotImplementedError
+    def get_game_session_status(self, session_id: int) -> GameSessionStatus:
+        status=GameSessionModel.objects.get(id=session_id).status
+        return status
 
     def get_pieces(self, session_id: int) -> List[PieceEntity]:
-        positions = PieceModel.objects.filter(game_session=session_id)
+        positions=[]
+        # достаем элементы фигур
+        pieces_of_session = PieceModel.objects.filter(game_session=session_id)
+        for el in pieces_of_session:
+            positions.append(el.position)
         return positions
 
     def get_possible_moves(self, piece_id: int) -> List[str]:
@@ -38,13 +40,15 @@ class EngineService:
         return possible_moves
 
 
-    def move_piece(self, piece_id: int, position: str) -> List[PieceEntity]:
+    def move_piece(self, piece_id: int, position: str):
         # получим позицию фигуры по id
         start_position=PieceModel.objects.get(id=piece_id).position
+        # получим цвет фигуры
+        piece_colour=PieceModel.objects.get(id=piece_id).color
         # получим игровую сессию фигуры по id
         session=PieceModel.objects.get(id=piece_id).game_session
         # получим id игрока
-        player=PlayerGameSessionModel.objects.get(session_id=session).player_id
+        player=PlayerGameSessionModel.objects.get(session_id=session, color=piece_colour).player_id
         # получим список положения фигур [[type, colour, position, game_session], ... ] для создания объекта доски
         positions=PieceModel.objects.filter(game_session=session)
         # создаем доску для игры
@@ -54,36 +58,51 @@ class EngineService:
         # съеденных фигур и особых операций в виде повышения пешки и рокировки нет, если len(res)=0
         if len(res)==0:
             # изменим значение position у фигуры
-            PieceModel.objects.get(id=piece_id).position=position
+            piece=PieceModel.objects.get(id=piece_id)
+            piece.position=position
         # res - список данных для изменения БД
         # съедение фигуры
         elif len(res)==1:
             #добавим очки игроку
-            #Itogs.objects.get(id_session=session, id_player=player).points+=int(res[0].value)
+            #player_points=Itogs.objects.get(id_session=session, id_player=player)
+            #player_points.points+=int(res[0].value)
             # изменим значение position у фигуры
-            PieceModel.objects.get(id=piece_id).position = position
+            piece=PieceModel.objects.get(id=piece_id)
+            piece.position = position
             # удалить из бд съеденную фигуру данной сессии, указанного цвета и типа
             PieceModel.objects.filter(game_session=session, type=res[0].get_type().name, color=res[0].get_colour().name).delete()
             # если съеден король, поменять статус игры
             if res[0].get_type()==PieceType.KING:
-                GameSessionModel.objects.filter(id=session).status = GameSessionStatus.COMPLETED
-            # у проигравших снять очки
+                game_status=GameSessionModel.objects.filter(id=session)
+                game_status.status = GameSessionStatus.COMPLETED
+                """
+                # у остальных игроков (проигравших) снять 50 очков
+                
+                for i in range(2):
+                    piece_colour=self.get_next_colour(piece_colour)
+                    loser_id=PlayerGameSessionModel.objects.get(color=piece_colour, session_id=session)
+                    loser_points=Itogs.objects.get(id_session=session, id_player=loser_id)
+                    loser_points.points-=50
+                """
         # рокировка
         elif len(res)==2:
             # изменение position для короля
-            PieceModel.objects.get(id=piece_id).position = position
+            king_piece=PieceModel.objects.get(id=piece_id)
+            king_piece.position = position
             # изменим position для ладьи
-            PieceModel.objects.get(game_session=session, type=res[0].get_type().name, color=res[0].get_colour().name)\
-                .position=res[1]
+            rook_piece=PieceModel.objects.get(game_session=session, type=res[0].get_type().name, color=res[0].get_colour().name)
+            rook_piece.position=res[1]
         # повышение пешки
         elif len(res)==3:
             # меняем тип фигуры на QUEEN
-            PieceModel.objects.get(id=piece_id).type=PieceType.QUEEN
-        # поменять статус игрока данной сессии и текущего цвета на ожидание
-        PlayerGameSessionModel.objects.get(session_id=session, color=PieceModel.objects.get(id=piece_id).color).status=PlayerStatus.WAIT
+            pawn_piece=PieceModel.objects.get(id=piece_id)
+            pawn_piece.type=PieceType.QUEEN
+        # поменять статус игрока данной сессии из текущего цвета на ожидание
+        cur_player=PlayerGameSessionModel.objects.get(session_id=session, color=PieceModel.objects.get(id=piece_id).color)
+        cur_player.status=PlayerStatus.WAIT
         # поменять статус следующего игрока на совершение хода
-        PlayerGameSessionModel.objects.get(session_id=session, color=self.get_next_colour(PieceModel.objects.get(id=piece_id).color))\
-            .status=PlayerStatus.CURRENT
+        next_player=PlayerGameSessionModel.objects.get(session_id=session, color=self.get_next_colour(PieceModel.objects.get(id=piece_id).color))
+        next_player.status=PlayerStatus.CURRENT
         # если ход невозможен, вызываем исключение
 
     def get_next_colour(self, cur_col):
