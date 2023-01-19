5f6309775861bf3c3ace568d1585648b7a785972
Reversean <odaving111@gmail.com>
2023-01-19 12:21:30 +0300

    Websocket infrastructure

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,18 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import app.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            app.routing.websocket_urlpatterns
+        )
+    )
+})
