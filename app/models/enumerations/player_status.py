dafc32d053c125d2d9a78f2f1b6d7eebdc38dcb9
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-26 14:21:54 +0300

    changing_enum_elements
    Изменение enum элементов в связи с изменением архитектуры

--- app/models/enumerations/player_status.py
+++ app/models/enumerations/player_status.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
 class PlayerStatus(models.TextChoices):
-    CONNECTING = _("connecting")
-    CONNECTED = _("connected")
-    WAIT = _("wait")
-    CURRENT = _("current")
-    ELIMINATED = _("eliminated")
-    DISCONNECTED = _("disconnected")
+    WAIT_TURN = _("wait_turn")
+    CURRENT_TURN = _("current_turn")
+    LOSE = _("lose")
+    WIN = _("win")
 
