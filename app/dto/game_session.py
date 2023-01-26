9126b26276258361a8a8c91dacbeee0cb14987a1
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-26 19:20:12 +0300

    Creating_GameSession

---
+++ app/dto/game_session.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from ..models.enumerations.game_session_status import GameSessionStatus
+
+
+@dataclass
+class GameSession:
+    id: int
+    status: GameSessionStatus
