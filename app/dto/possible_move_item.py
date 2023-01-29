9f6ac9c97ce54241a457f173bc765f16c96ef519
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 02:23:44 +0300

    Update and rename possible_move_item.py to possible_moves_item.py

--- app/dto/possible_move_item.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-from ..models.enumerations.position import Position
-
-@dataclass
-class PossibleMoveItem:
-    value: Position
---
+++ app/dto/possible_moves_item.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class PossibleMovesItem:
+    piece_id: int
+    possible_moves: list
