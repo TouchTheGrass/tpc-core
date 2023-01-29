b1dcb51722f5a5d693ec3ac59a9872ff7c847a3c
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 02:16:11 +0300

    creating_game_service
    Создание сервиса для работы с быстроизменяющимися данными

---
+++ app/services/games.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from app.models.chess_classes.piece_type import PieceTypeEngine
+from app.models.chess_classes.color import PieceColorEngine
+from app.models.enumerations.player_status import PlayerStatus
+
+@dataclass
+class GameInfo:
+    players: list
+    pieces: list
+
+@dataclass
+class Piece:
+    game_session_id: int
+    type: PieceTypeEngine
+    color:PieceColorEngine
+    position: str
+
+@dataclass
+class PlayerInfo:
+    user_id: int
+    game_session_id: int
+    player_status: PlayerStatus
+
+games={}
+
+
+
+
