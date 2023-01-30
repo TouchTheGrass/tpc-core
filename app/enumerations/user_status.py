7db4f2ea26b68b9ef686fa14dc085a50f88c1954
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 23:13:56 +0300

    Update and rename player_status.py to user_status.py

--- app/enumerations/user_status.py
+++ app/enumerations/user_status.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class PlayerStatus(models.TextChoices):
+class UserStatus(models.TextChoices):
     NOT_READY = _("not_ready")
     READY = _("ready")
     PLAYING = _("playing")
