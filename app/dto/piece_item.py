2c1f91e8e432b1c60c1b3fbd89972eb48f4a4c44
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 14:44:59 +0300

    Update piece_item.py

--- app/dto/piece_item.py
+++ app/dto/piece_item.py
@@ -1,11 +1,9 @@
 from dataclasses import dataclass
-from ..models.enumerations.piece_type import PieceType
-from ..models.enumerations.piece_color import PieceColor
-from ..models.enumerations.position import Position
+
 
 @dataclass
 class PieceItem:
     id: int
-    type: PieceType
-    color: PieceColor
-    position: Position
+    type: str
+    color: str
+    position: str
