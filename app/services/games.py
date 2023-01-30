04517b96a24413c4ea603d2b612b1ee80753528f
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 23:18:42 +0300

    Create games.py

---
+++ app/services/games.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from app.enumerations.piece_type import PieceType
+from app.enumerations.piece_color import PieceColor
+from app.enumerations.player_status import PlayerStatus
+
+@dataclass
+class GameInfo:
+    players: list
+    pieces: list
+
+@dataclass
+class Piece:
+    id: int
+    game_session_id: int
+    type: PieceType
+    color:PieceColor
+    position: str
+
+@dataclass
+class PlayerInfo:
+    user_id: int
+    game_session_id: int
+    player_status: PlayerStatus
+
+games={}
+
+
+
+
