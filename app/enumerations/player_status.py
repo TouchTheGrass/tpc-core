335d5078a2ec9eb37d4f02e86c0e60a690c6d9dd
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 23:14:34 +0300

    Create player_status.py

---
+++ app/enumerations/player_status.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class PlayerStatus(models.TextChoices):
+    WAIT_TURN = _("wait_turn")
+    CURRENT_TURN = _("current_turn")
+    LOSE = _("lose")
+    WIN = _("win")
