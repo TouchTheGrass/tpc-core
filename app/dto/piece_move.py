2f8d8222544e2041c981853280f9e61d7bcfd7e3
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 14:45:22 +0300

    Update piece_move.py

--- app/dto/piece_move.py
+++ app/dto/piece_move.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
-from ..models.enumerations.piece_type import PieceType
-from ..models.enumerations.position import Position
+
 
 @dataclass
 class PieceMove:
     id: int
-    position:Position
+    position:str
