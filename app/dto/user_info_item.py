5f73174a30837f9022caa967485d79ab84bfd556
EVolkova99 <116887618+EVolkova99@users.noreply.github.com>
2023-01-30 14:47:16 +0300

    Update user_info_item.py

--- app/dto/user_info_item.py
+++ app/dto/user_info_item.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-from ..models.enumerations.color import Color
-from ..models.enumerations.user_status import UserStatus
+
 
 @dataclass
 class UserInfoItem:
     id: int
     name: str
-    status: UserStatus
-    color: Color
+    status: str
+    rating: int
+    color: str
